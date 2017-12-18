c68381cfe55064fe52f6699eb9489605c23ca2ae
AHN HO JUNG <labeldock@me.com>
2017-12-18 16:36:28 +0900

    update

--- unix-init.sh
+++ unix-init.sh
@@ -1,22 +1,48 @@
-echo "ACTIVATE CUSTOM UNIX CONFIG"
+echo "ACTIVATE CUSTOM UNIX CONFIG ($HOME/config/unix-source/)"
 
+# setting
 LINK_TARGET_FILES=".gitconfig .vimrc .tmux.conf .tm_properties"
 
-function config {
+function configbootstrap {
 
-echo -e "1.git user"
-read selected
+# 미리 정의해놓은 설정파일들 링크 처리
+for FILE_ITEM in $LINK_TARGET_FILES
+do
 
-if [ $selected -eq "1" ]
+if [ ! -h $HOME/$FILE_ITEM ]
 then
-    echo "user email"
-    read email
-    echo "user name"
-    read name
-    
-    git config user.name "$name"
-    git config user.email "$email"
+echo "link file $HOME/config/unix-source/$FILE_ITEM"
+ln -sf $HOME/config/unix-source/$FILE_ITEM $HOME/$FILE_ITEM
 fi
+done
 
 }
 
+
+# command
+function config {
+
+echo -e "ENTER COMMAND\n[0!] remove your all config \n[1] git user"
+read selected
+
+case "$selected" in
+    "0!")
+        for FILE_ITEM in $LINK_TARGET_FILES
+        do
+            rm $HOME/$FILE_ITEM
+            echo "rm $FILE_ITEM"
+        done
+        configbootstrap
+    ;;
+    "1")
+        echo "user email"
+        read email
+        echo "user name"
+        read name
+
+        git config user.name "$name"
+        git config user.email "$email"
+    ;;
+esac
+
+}
