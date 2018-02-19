f7f804c77c84ef28057dff36ae0b6ee13cbf9865
AHN HO JUNG <labeldock@me.com>
2018-02-19 14:53:36 +0900

    one step install vundle

--- unix-init.sh
+++ unix-init.sh
@@ -7,20 +7,21 @@ echo "ACTIVATE CUSTOM CONFIG ($HOME/config/unix-source/)"
 LINK_TARGET_FILES=".gitconfig .vim .vimrc .tmux.conf .tm_properties"
 
 function configbootstrap {
+    # 미리 정의해놓은 설정파일들 링크 처리
+    for FILE_ITEM in $LINK_TARGET_FILES
+    do
 
-# 미리 정의해놓은 설정파일들 링크 처리
-for FILE_ITEM in $LINK_TARGET_FILES
-do
-
-if [ ! -h $HOME/$FILE_ITEM ]
-then
-echo "link file $HOME/config/unix-source/$FILE_ITEM"
-ln -sf $HOME/config/unix-source/$FILE_ITEM $HOME/$FILE_ITEM
-fi
-done
-
+    if [ ! -h $HOME/$FILE_ITEM ]
+    then
+    echo "link file $HOME/config/unix-source/$FILE_ITEM"
+    ln -sf $HOME/config/unix-source/$FILE_ITEM $HOME/$FILE_ITEM
+    fi
+    done
 }
 
+function vundleinstall {
+    vim +PluginInstall +qall
+}
 
 # command
 function config {
@@ -39,6 +40,9 @@ case "$selected" in
             echo "removed $HOME/config/unix-source"
         fi
         
+        rm -rf "$HOME/config/unix-source.default/.vim/bundle/Vundle.vim"
+        git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/config/unix-source.default/.vim/bundle/Vundle.vim/"
+        
         cp -rf "$HOME/config/unix-source.default" "$HOME/config/unix-source"
         echo "copyed $HOME/config/unix-source.default to $HOME/config/unix-source"
         
@@ -49,6 +53,8 @@ case "$selected" in
         done
         
         configbootstrap
+        
+        vundleinstall
     ;;
     "r!")
         source "$HOME/config/unix-init.sh"
@@ -83,7 +89,7 @@ case "$selected" in
         fi
     ;;
     "vundle")
-        vim +PluginInstall +qall
+        vundleinstall
     ;;
     "nvm")
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
