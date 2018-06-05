0399787288e6b8105bdd8bd156c32e2b7bfaf00a
AHN HO JUNG <labeldock@me.com>
2018-06-05 21:20:51 +0900

    new version

--- unix-functions.sh
+++ unix-functions.sh
@@ -1,5 +1,6 @@
 # setting
-LINK_TARGET_FILES=".gitconfig .vim .vimrc .tmux.conf .tm_properties"
+CONFIG_TEMPLATES_PATH="$HOME/config/dotfiles.templates"
+CONFIG_ACTIVE_PATH="$HOME/config/dotfiles.active"
 
 # read_val "프롬프트 질문 내용" "변수이름"
 read_val() {
@@ -21,16 +22,13 @@ default_with_read_val() {
 }
 
 
-CONFIG_TEMPLATES_PATH=$HOME/config/dotfiles.templates
-CONFIG_ACTIVE_PATH=$HOME/config/dotfiles.active
-
 configRemoveActivedDotfils() {
   if [[ -z $1 ]]; then
     return 1
   fi
   
-  rm $CONFIG_ACTIVE_PATH/$1
-  rm $HOME/$1
+  rm -rf $CONFIG_ACTIVE_PATH/$1
+  rm -rf $HOME/$1
   
   echo "link file $CONFIG_ACTIVE_PATH/$1"
 }
@@ -39,12 +37,42 @@ configCopyAndLinkDotfils() {
   if [[ -z $1 ]]; then
     return 1
   fi
+  configRemoveActivedDotfils $1
   
-  cp $CONFIG_TEMPLATES_PATH/$1 $CONFIG_ACTIVE_PATH/$1
-  echo "copy file $CONFIG_ACTIVE_PATH/$1"
+  if [[ -d "$CONFIG_TEMPLATES_PATH/$1" ]]; then
+    cp -rf $CONFIG_TEMPLATES_PATH/$1 $CONFIG_ACTIVE_PATH/$1
+    echo "copy directory $CONFIG_ACTIVE_PATH/$1"
+  else
+    cp $CONFIG_TEMPLATES_PATH/$1 $CONFIG_ACTIVE_PATH/$1
+    echo "copy file $CONFIG_ACTIVE_PATH/$1"
+  fi
   
   ln -sf $CONFIG_ACTIVE_PATH/$1 $HOME/$1
-  echo "link file $CONFIG_ACTIVE_PATH/$1"
+  echo "link file $CONFIG_ACTIVE_PATH/$1 => $HOME/$1"
+}
+
+#softtab $1 = softtab $2 = $tabsize
+configTmPropertiesReadVal() {
+  local softtab
+  local tabsize
+  read -p "TextMate::softtab?[true/false]" softtab
+  read -p "TextMate::tabsize?(2/4/...)" tabsize
+  [[ ! -z $softtab ]] && eval "$1='$softtab'"
+  [[ ! -z $softtab ]] && eval "$2='$tabsize'"
+}
+
+# $1 = path $2 = $softtab $3 = $tabsize
+configTmPropertiesSet(){
+  if [ $2 ]
+  then
+    echo "softWrap=$2" >> "$1"
+    echo "softTabs=$2" >> "$1"
+  fi
+      
+  if [ $3 ]
+  then
+    echo "tabSize=$3" >> "$1"
+  fi
 }
 
 
@@ -55,21 +83,35 @@ function configunixfunctions {
 
   case "$selected" in
   "i!")
-    read_val "Do you want to install all of them automatically?[y]" automatically
-    [[ $automatically =~ ^(y|Y)$ ]] && automatically=y || automatically=false
+    # automatic install
+    local AUTOMATIC_INSTALL
+    read_val "Do you want to install all of them AUTOMATIC_INSTALL?[y]" AUTOMATIC_INSTALL
+    [[ $AUTOMATIC_INSTALL =~ ^(y|Y)$ ]] && AUTOMATIC_INSTALL=y || AUTOMATIC_INSTALL=false
+    
     
     # setup vundle
-    default_with_read_val setupGitConfig $automatically "Do you setup setup .gitconfig?[y]"
-    default_with_read_val setupVimVundle $automatically "Do you setup .vimrc and vundle?[y]"
-    default_with_read_val setupTmuxConfig $automatically "Do you setup setup .tmux.conf?[y]"
-    default_with_read_val setupTmProperties $automatically "Do you setup .tm_properties?[y]"
+    local SHOULD_SETUP_GIT
+    local SHOULD_SETUP_VIM
+    local SHOULD_SETUP_TMUX
+    local SHOULD_SETUP_TM_PROPERTIES
+    default_with_read_val SHOULD_SETUP_GIT $AUTOMATIC_INSTALL "Do you setup .gitconfig?[y]"
+    default_with_read_val SHOULD_SETUP_VIM $AUTOMATIC_INSTALL "Do you setup .vimrc and vundle?[y]"
+    default_with_read_val SHOULD_SETUP_TMUX $AUTOMATIC_INSTALL "Do you setup setup .tmux.conf?[y]"
+    default_with_read_val SHOULD_SETUP_TM_PROPERTIES $AUTOMATIC_INSTALL "Do you setup .tm_properties?[y]"
+    
+    
+    #
+    local TM_SOFTTAB=true
+    local TM_TABSIZE=2
+    if [[ $SHOULD_SETUP_TM_PROPERTIES == "y" && $AUTOMATIC_INSTALL == false ]]; then
+      configTmPropertiesReadVal TM_SOFTTAB TM_TABSIZE
+    fi
+    
     
     # backup legacy dotfiles
     local UTIME=$(date +%s)
     if [[ -d $HOME/config/dotfiles.active ]]; then
       cp -rf "$HOME/config/dotfiles.active" "$HOME/config/dotfiles.backup.$UTIME"
-      rm -rf "$HOME/config/dotfiles.active"
-      echo "removed $HOME/config/dotfiles.active"
     fi
     
     
@@ -78,55 +120,52 @@ function configunixfunctions {
     fi
     
     
-    
     # .gitconfig
-    if [[ $setupGitConfig == 'y' ]]; then
+    if [[ $SHOULD_SETUP_GIT == 'y' ]]; then
       configCopyAndLinkDotfils ".gitconfig"
-    else
-      echo "Sorry. Custom setup is not ready yet, but it will work soon."
     fi
     
     
     # git, vundle
-    if [[ $setupVimVundle == 'y' ]]; then
-      configCopyAndLinkDotfils ".vimrc"
+    if [[ $SHOULD_SETUP_VIM == 'y' ]]; then
+      rm -rf "$CONFIG_TEMPLATES_PATH/.vim/bundle/Vundle.vim"
+      git clone https://github.com/VundleVim/Vundle.vim.git "$CONFIG_TEMPLATES_PATH/.vim/bundle/Vundle.vim/"
       
-      rm -rf "$HOME/config/dotfiles.templates/.vim/bundle/Vundle.vim"
-      git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/config/dotfiles.templates/.vim/bundle/Vundle.vim/"
+      sleep 2
+      configCopyAndLinkDotfils ".vimrc"
+      configCopyAndLinkDotfils ".vim"
+      
       vim +PluginInstall +qall
       echo "install complete vundle"
-    else
-      echo "Sorry. Custom setup is not ready yet, but it will work soon."
     fi
     
     
     # setup tmux
-    if [[ $setupTmuxConfig == 'y' ]]; then
+    if [[ $SHOULD_SETUP_TMUX == 'y' ]]; then
       configCopyAndLinkDotfils ".tmux.conf"
       tmux source-file ~/.tmux.conf
-    else
-      echo "Sorry. Custom setup is not ready yet, but it will work soon."
     fi
     
     
     # setup textmate
-    if [[ $setupTmProperties == 'y' ]]; then
+    if [[ $SHOULD_SETUP_TM_PROPERTIES == 'y' ]]; then
       configCopyAndLinkDotfils ".tm_properties"
-    else
-      echo "Sorry. Custom setup is not ready yet, but it will work soon."
+      echo "tmSoftTab, tmTabSize = ${TM_SOFTTAB}, ${TM_TABSIZE}"
+      echo "softWrap=${TM_SOFTTAB}" >> "$CONFIG_ACTIVE_PATH/.tm_properties"
+      echo "softTabs=${TM_TABSIZE}" >> "$CONFIG_ACTIVE_PATH/.tm_properties"
     fi
 
     echo "complete config i!"
     ;;
   "gu")
+    local name
+    local email
     echo "user name"
     read name
     echo "user email"
     read email
-        
     git config user.name "$name"
     git config user.email "$email"
-        
     git config --list | grep "user."
     ;;
   "gc")
@@ -144,24 +183,13 @@ function configunixfunctions {
     git config --list | grep "credential"
     ;;
   "tmp")
-    if [ ! -h $PWD/.tm_properties ]
-    then
+    if [[ ! -h $PWD/.tm_properties ]]; then
       touch .tm_properties
     fi
-        
-    read -p "usesofttab?[true/false]" softtab
-    read -p "tabsize?(2/4/...)" tabsize
-        
-    if [ $softtab ]
-    then
-      echo "softWrap=$softtab" >> "$PWD/.tm_properties"
-      echo "softTabs=$softtab" >> "$PWD/.tm_properties"
-    fi
-        
-    if [ $tabsize ]
-    then
-      echo "tabSize=$tabsize" >> "$PWD/.tm_properties"
-    fi
+    local softtab
+    local tabsize
+    configTmPropertiesReadVal softtab tabsize
+    configTmPropertiesSet "$PWD/.tm_properties" softtab tabsize
     ;;
   "rvm")
     curl -sSL https://get.rvm.io | bash -s stable
