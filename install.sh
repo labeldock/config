d051d80de2aed60d7d07bcccde2687a7e13725f6
PCA\label <labeldock@me.com>
2023-03-05 16:30:13 +0900

    bin style draft

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+SHELL_CONFIGURATION=
+FLAG_CONFIGURATION="#~/config/bin:path:flag"
+PATH_CONFIGURATION='[ -d "$HOME/config/bin" ] && PATH="$HOME/config/bin:$PATH" '$FLAG_CONFIGURATION
+
+echo "Checking shell environment..."
+if echo $SHELL | grep -q "zsh"; then
+  echo "Detected zsh shell. Using ~/.zshrc for configuration."
+  SHELL_CONFIGURATION=~/.zshrc
+elif echo $SHELL | grep -q "bash"; then
+  echo "Detected bash shell. Using ~/.bashrc for configuration."
+  SHELL_CONFIGURATION=~/.bashrc
+else
+  echo "Unsupported shell environment. Exiting..."
+  exit 1
+fi
+
+echo "Checking if .*rc exists. Creating it if not..."
+if [ ! -f $SHELL_CONFIGURATION ]; then
+  touch $SHELL_CONFIGURATION
+fi
+
+echo "Checking if PATH_CONFIGURATION exists in .*rc..."
+if grep -q "$FLAG_CONFIGURATION" $SHELL_CONFIGURATION; then
+  existing_line=$(grep "$FLAG_CONFIGURATION" $SHELL_CONFIGURATION)
+  if [ "$existing_line" != "$PATH_CONFIGURATION" ]; then 
+    echo "Replacing existing line with PATH_CONFIGURATION..."
+    awk -v existing="$existing_line" -v target="$PATH_CONFIGURATION" '{ if ($0 == existing) { $0=target } print }' $SHELL_CONFIGURATION > temp && mv temp $SHELL_CONFIGURATION
+  else
+    echo "Great job! It looks like the installation is already completed."
+  fi
+else
+  echo "Appending PATH_CONFIGURATION to $SHELL_CONFIGURATION..."
+  echo "$PATH_CONFIGURATION" >> $SHELL_CONFIGURATION
+fi
---
+++ lib/helper.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+read_nth() {
+  local n=$1
+  shift
+  local string="$*"
+  local words=($string)
+  if [[ $n -gt 0 && $n -le ${#words[@]} ]]; then
+    echo "${words[$((n-1))]}"
+  fi
+}
+
+ask_nth () {
+  local var_name=$1
+  shift
+  local options=("$@")
+  local num_options=${#options[@]}
+  
+  for i in $(seq 1 $num_options); do
+    echo "$i) ${options[$i-1]}"
+  done
+  
+  while true; do
+    read -p "Enter your choice: " choice
+    for i in $(seq 1 $num_options); do
+      if [[ "$choice" == "$i" || "$choice" == "${options[$i-1]}" ]]; then
+        choice=$i
+        break 2
+      fi
+    done
+    echo "Invalid choice. Please enter a valid option."
+  done
+  
+  eval "$var_name=$choice"
+}
+
+call_nth() {
+  local n="$1"
+  shift
+  local funcs=("$@")
+  
+  if (( n < 1 || n > ${#funcs[@]} )); then
+    echo "Run number is incorrect."
+    exit 1
+  fi
+  
+  local func="${funcs[$((n-1))]}"
+  if [[ "$(type -t "$func")" != "function" ]]; then
+    echo "[$func] is not an executable function"
+    exit 1
+  fi
+  
+  "$func"
+}
+
+call_all() {
+  for arg in "$@"; do
+    "$arg"
+  done
+}
+
+eval_keys() {
+  local -n map=$1
+  for key in $(echo "${!map[@]}" | tr ' ' '\n' | sort); do
+    echo "$key"
+  done
+}
+
+eval_values() {
+  local -n map=$1
+  for key in $(echo "${map[@]}" | tr ' ' '\n' | sort); do
+    echo "$key"
+  done
+}
+
+eval_nth() {
+    local index=$1
+    shift
+    local arr=("$@")
+    echo "${arr[$((index-1))]}"
+}
